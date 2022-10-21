128d327ac5e87d66da584677879e556c9e01f42a
rahmaliabaani <rahmaliarpla15@gmail.com>
2022-10-21 21:58:35 +0700

    Video#3 Struktur Folder, Routes, & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rahmalia Nuursyabaani",
+        "email" => "203040084@mail.unpas.ac.id",
+        "image" => "foto.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
