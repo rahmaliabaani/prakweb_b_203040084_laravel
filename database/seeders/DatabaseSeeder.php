26b6297aeb21a6b812072ccb1628239aa22e2d9c
rahmaliabaani <rahmaliarpla15@gmail.com>
2022-10-28 23:58:20 +0700

    Video#9 Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +24,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Rahmalia Nuursyabaani',
+            'email' => 'rahma@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Bunga Fajrin',
+            'email' => 'bunga@gmail.com',
+            'password' => bcrypt('54321')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia sint nulla iusto laudantium, delectus excepturi minus obcaecati necessitatibus dignissimos!',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia sint nulla iusto laudantium, delectus excepturi minus obcaecati necessitatibus dignissimos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatum cupiditate inventore iure ea pariatur similique animi optio possimus repudiandae, eius, aut iusto ipsa. Porro temporibus consequatur a ipsum commodi ab dolorum. Modi, incidunt temporibus earum error tempora fugit, mollitia voluptates odio nemo ad at vel dolorem inventore perspiciatis ullam!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia sint nulla iusto laudantium, delectus excepturi minus obcaecati necessitatibus dignissimos!',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia sint nulla iusto laudantium, delectus excepturi minus obcaecati necessitatibus dignissimos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatum cupiditate inventore iure ea pariatur similique animi optio possimus repudiandae, eius, aut iusto ipsa. Porro temporibus consequatur a ipsum commodi ab dolorum. Modi, incidunt temporibus earum error tempora fugit, mollitia voluptates odio nemo ad at vel dolorem inventore perspiciatis ullam!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia sint nulla iusto laudantium, delectus excepturi minus obcaecati necessitatibus dignissimos!',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia sint nulla iusto laudantium, delectus excepturi minus obcaecati necessitatibus dignissimos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatum cupiditate inventore iure ea pariatur similique animi optio possimus repudiandae, eius, aut iusto ipsa. Porro temporibus consequatur a ipsum commodi ab dolorum. Modi, incidunt temporibus earum error tempora fugit, mollitia voluptates odio nemo ad at vel dolorem inventore perspiciatis ullam!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia sint nulla iusto laudantium, delectus excepturi minus obcaecati necessitatibus dignissimos!',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia sint nulla iusto laudantium, delectus excepturi minus obcaecati necessitatibus dignissimos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatum cupiditate inventore iure ea pariatur similique animi optio possimus repudiandae, eius, aut iusto ipsa. Porro temporibus consequatur a ipsum commodi ab dolorum. Modi, incidunt temporibus earum error tempora fugit, mollitia voluptates odio nemo ad at vel dolorem inventore perspiciatis ullam!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,11 +2,12 @@
 @extends('layouts.main')
 
 @section('container')
-<article>
-    <h2>{{ $post->title }}</h2>
-    <p>By. Rahmalia Nuursya'baani in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-    {!! $post->body !!}
-</article>
 
-    <a href="/blog">Back to Posts</a>
+    <h2 class="mb-3">{{ $post->title }}</h2>
+
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    
+    {!! $post->body !!}
+
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
