bd936422c1ec802d2dddc5ce6e909e7574ef9295
rahmaliabaani <rahmaliarpla15@gmail.com>
2022-10-28 14:56:05 +0700

    Video#8 Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,7 @@
 @section('container')
 <article>
     <h2>{{ $post->title }}</h2>
+    <p>By. Rahmalia Nuursya'baani in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 </article>
 
