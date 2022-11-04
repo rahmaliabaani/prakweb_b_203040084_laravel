889f780fe87fb34afe33ab6e8b22a72f1908a183
rahmaliabaani <rahmaliarpla15@gmail.com>
2022-11-04 18:23:56 +0700

    Video#13 Searching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => 'about',
         "name" => "Rahmalia Nuursyabaani",
         "email" => "203040084@mail.unpas.ac.id",
         "image" => "foto.png"
@@ -42,20 +44,5 @@ Route::get('/categories', function() {
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
